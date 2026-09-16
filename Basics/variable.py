35e8e0de8559011066a6ef5486bfc5a6445e88ae
Aditya-Sasne <sasneaditya2004@gmail.com>
2026-09-16 21:14:59 +0530

    variable program added

---
+++ Basics/variable.py
@@ -0,0 +1,21 @@
+emp_id = 2001
+emp_name = "aditya sasne"
+emp_salary = 6.5
+emp_eqn = 3+2j
+emp_bool = True
+
+print(type(emp_id))
+print(type(emp_name))
+print(type(emp_salary))
+print(type(emp_eqn))
+print(type(emp_bool))
+
+print("Employee_id : ",emp_id)
+print("Employee_name : ",emp_name)
+print("Employee_salary : ",emp_salary)
+print("Employee_equation : ",emp_eqn)
+print("Employee_boolean : ",emp_bool)
+
+print(emp_name[0])
+print(emp_name[10])
+print(emp_name[-2])
