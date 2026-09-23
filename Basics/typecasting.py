ef8112455f0ddc5f6153643cc48d56cb3c52fd9d
Aditya-Sasne <sasneaditya2004@gmail.com>
2026-09-23 11:29:09 +0530

    typecasting added

---
+++ Basics/typecasting.py
@@ -0,0 +1,22 @@
+#explicit typecasting
+
+a = "6"
+print(type(a))
+b = "7"
+print(type(b))
+
+c = int(a) + int (b)
+print(c)
+print(type(c))
+
+
+#implicit typecasting
+
+x = 8
+print(type(x))
+y = 15.5
+print(type(y))
+
+z = x + y
+print(z)
+print(type(y))
