f656c5ec093119efb95f1a4ddf5db6e6564fd258
Aditya-Sasne <sasneaditya2004@gmail.com>
2026-09-23 12:26:48 +0530

    elif added

---
+++ control_statements/elif.py
@@ -0,0 +1,12 @@
+day = input("Enter day")
+
+if day == "Monday" or day == "Mon" :
+    print("Week started")
+elif day == "Friday" or day == "Fri" :
+    print("Weekend is coming")
+elif day == "Saturday" or day == "Sat" :
+    print("Weekend Started")
+elif day == "Sunday" or day == "Sun" : 
+    print("Relax, it's Sunday")
+else :
+    print("Just another day")
